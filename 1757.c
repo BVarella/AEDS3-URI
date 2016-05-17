1eae8805418793d7731055e8ba7ab1b9ae175f16
Bruno Varella Peixoto <varellap@gmail.com>
2016-05-17 11:35:26 -0300

    Primeiro teste 70% wrong answer

--- 1757.c
+++ 1757.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(){
 
 	int numTestes, numCidades, numVisiveis, a, b;
-	int i;
+	int i, j;
 	int cidades[100];
+	int result;
 
 	scanf("%d", &numTestes);
 	for(i = 0; i < numTestes; ++i){
+		memset (cidades, 0, sizeof(cidades));
+		result = 0;
 		scanf("%d %d", &numCidades, &numVisiveis);
-		
+		for(j = 0; j < numVisiveis; ++j){
+			scanf("%d %d", &a, &b);
+			cidades[a] += 1;
+			cidades[b] += 1;
+		}
+		for(j = 0; j < numCidades; ++j){
+			if(cidades[j] == numCidades-1)
+				++result;
+		}
+		printf("%d\n", result);
 	}
 
 	return 0;
