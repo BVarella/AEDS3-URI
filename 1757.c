4d525da494dc1ad1b7bb265f8eae618ce90d0d11
Bruno Varella Peixoto <varellap@gmail.com>
2016-05-17 22:07:17 -0300

    Final

--- 1757.c
+++ 1757.c
@@ -1,93 +1,65 @@
-// A C / C++ program for Prim's Minimum Spanning Tree (MST) algorithm. 
-// The program is for adjacency matrix representation of the graph
- 
 #include <stdio.h>
-//#include <limits.h>
-//#include <stdbool.h>
- 
-// Number of vertices in the graph
-int MAX = 100;
+
 #define INF 2147483647;
 
-// A utility function to find the vertex with minimum key value, from
-// the set of vertices not yet included in MST
-int minKey(int key[], int mstSet[], int V)
-{
-   // Initialize min value
-   int min = INF;
-   int min_index;
- 	int v;
-   for (v = 0; v < V; v++)
-	 if (mstSet[v] == 0 && key[v] < min)
-		 min = key[v], min_index = v;
- 
-   return min_index;
+int MAX = 100;
+
+int minKey(int key[], int mstSet[], int V){
+
+	int min = INF;
+	int min_index;
+	int v;
+	
+	for (v = 0; v < V; v++)
+		if (mstSet[v] == 0 && key[v] < min)
+			min = key[v], min_index = v;
+
+	return min_index;
 }
- 
-// A utility function to print the constructed MST stored in parent[]
-void printMST(int parent[], int n, int graph[MAX][MAX])
-{
+
+void cost(int parent[], int n, int graph[MAX][MAX]){
+	
 	int result = 0;
-   //printf("Edge   Weight\n");
+
 	int i;
-   for (i = 1; i < n; i++){
-	  //printf("%d - %d    %d \n", parent[i], i, graph[i][parent[i]]);
-   		if (graph[i][parent[i]] == 2)
-   			++result;
-   }
-   printf("%d\n", result);
+	for (i = 1; i < n; i++){
+		if (graph[i][parent[i]] == 2)
+			++result;
+	}
+
+	printf("%d\n", result);
 
 }
- 
-// Function to construct and print MST for a graph represented using adjacency
-// matrix representation
-void primMST(int graph[MAX][MAX], int V)
-{
-	 int parent[V]; // Array to store constructed MST
-	 int key[V];   // Key values used to pick minimum weight edge in cut
-	 int mstSet[V];  // To represent set of vertices not yet included in MST
- 
-	 // Initialize all keys as INFINITE
-	 int i;
-	 for (i = 0; i < V; i++){
+
+void prim(int graph[MAX][MAX], int V){
+	
+	int parent[V];
+	int key[V];
+	int mstSet[V];
+	int i, count, u, v;
+
+	for (i = 0; i < V; i++){
 		key[i] = INF;
 		mstSet[i] = 0;
 	}
- 
-	 // Always include first 1st vertex in MST.
-	 key[0] = 0;     // Make key 0 so that this vertex is picked as first vertex
-	 parent[0] = -1; // First node is always root of MST 
- 
-	 // The MST will have V vertices
-	 int count;
-	 for (count = 0; count < V-1; count++)
-	 {
-		// Pick thd minimum key vertex from the set of vertices
-		// not yet included in MST
-		int u = minKey(key, mstSet, V);
- 
-		// Add the picked vertex to the MST Set
+
+	key[0] = 0;
+	parent[0] = -1;
+
+	for (count = 0; count < V-1; count++){
+
+		u = minKey(key, mstSet, V);
+
 		mstSet[u] = 1;
- 
-		// Update key value and parent index of the adjacent vertices of
-		// the picked vertex. Consider only those vertices which are not yet
-		// included in MST
-		int v;
+
 		for (v = 0; v < V; v++)
- 
-		   // graph[u][v] is non zero only for adjacent vertices of m
-		   // mstSet[v] is false for vertices not yet included in MST
-		   // Update the key only if graph[u][v] is smaller than key[v]
-		  if (graph[u][v] && mstSet[v] == 0 && graph[u][v] <  key[v])
-			 parent[v]  = u, key[v] = graph[u][v];
-	 }
- 
-	 // print the constructed MST
-	 printMST(parent, V, graph);
+			if (graph[u][v] && mstSet[v] == 0 && graph[u][v] <  key[v])
+				parent[v]  = u, key[v] = graph[u][v];
+	}
+
+	cost(parent, V, graph);
 }
- 
- 
-// driver program to test above function
+
 int main(){
 
 	int numTestes, numCidades, numVisiveis, a, b;
@@ -112,12 +84,9 @@ int main(){
 			graph[b][a] = 2;
 		}
 
-	// Print the solution
-	primMST(graph, numCidades);
+	prim(graph, numCidades);
 
 	}
- 
 
- 
 	return 0;
 }
